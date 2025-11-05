fbd7eac452ed7c99c30ac5171612886dc1fc8bab
Grishma2477 <kandelgrishma2477@gmail.com>
2025-11-05 21:40:22 +1100

    :construction: media queries and pseudo selectors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,11 @@ import logo from '../assets/logo.png';
 
 export default function Header() {
   return (
-    <header>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+    <header className="flex flex-col items-center mt-8 mb-8 md:mb-16">
+      <img src={logo} alt="A canvas"
+       className="mb-8 w-44 h-44"/>
+      <h1 className="uppercase text - xl md: 4xl font semibold tracking widest center mdfont amber 800 title-xl md:text-center acking-widest mdfont-semibold md:text-4xl text-amber-800 font-title">ReactArt</h1>
+      <p className="text-stone-500">community of artists and art-lovers.</p>
     </header>
   );
 }
